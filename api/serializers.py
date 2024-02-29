869614e89b8ebb733bc00e1c06c0071d028f18f4
Viacheslav <fx.slav21@gmail.com>
2024-03-01 00:20:01 +0300

    Created serializers

---
+++ api/serializers.py
@@ -0,0 +1,85 @@
+from rest_framework import serializers, validators
+
+from api.models import ApiUser, Warehouse, Product, Order
+
+
+class UserSerializer(serializers.Serializer):
+    username = serializers.CharField(max_length=128, validators=[
+        validators.UniqueValidator(ApiUser.objects.all())
+    ])
+    email = serializers.EmailField()
+    position = serializers.ChoiceField(choices=ApiUser.POSITION)
+    password = serializers.CharField(min_length=6, max_length=20, write_only=True)
+
+    def create(self, validated_data):
+        user = ApiUser.objects.create(
+            email=validated_data["email"],
+            username=validated_data["username"],
+            position=validated_data["position"],
+        )
+        user.set_password(validated_data["password"])
+        user.save(update_fields=["password"])
+        return user
+
+    def update(self, instance, validated_data):
+        if email := validated_data.get("email"):
+            instance.email = email
+            instance.save(update_fields=["email"])
+
+        if password := validated_data.get("password"):
+            instance.set_password(password)
+            instance.save(update_fields=["password"])
+        return instance
+
+
+class ProductSerializer(serializers.ModelSerializer):
+    user = serializers.HiddenField(default=serializers.CurrentUserDefault())
+
+    class Meta:
+        model = Product
+        fields = "__all__"
+        extra_kwargs = {"id": {"read_only": True}}
+
+    def create(self, validated_data):
+        current_user = validated_data.get("user")
+        if current_user.position == "SU":
+            product = Product.objects.create(
+                name=validated_data["name"],
+                quantity=validated_data["quantity"],
+                warehouse=validated_data["warehouse"],
+                user=validated_data["user"]
+            )
+            product.save()
+            return product
+        else:
+            raise serializers.ValidationError("Customers are not authorized to create products")
+
+
+class WarehouseSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Warehouse
+        fields = "__all__"
+        extra_kwargs = {"id": {"read_only": True}}
+
+
+class OrderSerializer(serializers.ModelSerializer):
+    user = serializers.HiddenField(default=serializers.CurrentUserDefault())
+
+    class Meta:
+        model = Order
+        fields = "__all__"
+        extra_kwargs = {"id": {"read_only": True}}
+
+    def create(self, validated_data):
+        current_user = validated_data.get("user")
+        if current_user.position == "CO":
+            order = Order.objects.create(
+                product=validated_data["product"],
+                quantity=validated_data["quantity"],
+                warehouse=validated_data["warehouse"],
+                user=validated_data["user"]
+            )
+            order.save()
+            return order
+        else:
+            raise serializers.ValidationError("Suppliers are not authorized to create orders")
